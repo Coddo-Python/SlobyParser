72654a25e2993e22af96b11f71836611b90c20fa
Hadházy Tamás <helsgithub15@gmail.com>
2022-10-23 16:48:04 +0200

    Added things

--- slobyparser/lexer.py
+++ slobyparser/lexer.py
@@ -1,4 +1,9 @@
 
+# this project
+from slobyparser.tokens.tokens_type import Literals, Keywords, Operators, Identifiers
+from slobyparser.exceptions import SlobyException
+
+
 class Lexer:
     def __init__(self, text):
         self.text = text
@@ -6,6 +11,35 @@ class Lexer:
         self.current_char = None
         self.advance()
 
+        # tokens
+        self.literal = Literals()
+        self.keywords = Keywords()
+        self.operators = Operators()
+        self.identifiers = Identifiers()
+
+        self.token_types = [self.literal, self.keywords, self.operators, self.identifiers]
+
     def advance(self):
+
         self.pos += 1
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def find_token_type(self, token):
+        for token_type in self.token_types:
+            if token in token_type.ITEMS:
+                print(f"{token=}: {token_type.__class__.__name__}")
+                return token
+            else:
+                break
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char is not None:
+            if self.current_char in '\t':
+                self.advance()
+            if self.find_token_type(self.current_char):
+                tokens.append(self.current_char)
+                self.advance()
+            else:
+                raise SlobyException("Invalid")
--- slobyparser/token.py
+++ slobyparser/token.py
@@ -1,18 +1,46 @@
 from slobyparser.types.token_types import token_value, token_type
+import sys
 
 
-class Token(str):
-    __slots__ = ("type", "value")
+class Token:
+    __slots__ = ("type", "value", "token_position", "curr_pos", "end_pos", "start_pos")
     def __init__(self, type_: token_type = "", value: token_value = None):
         self.type = type_
         self.value = value
+
+        self.token_position = self.TokenPosition()
+
+        self.curr_pos = self.token_position.get_curr_position()
+
+        self.end_pos = self.token_position.get_end_position()
+        self.start_pos = self.token_position.get_start_position()
+
+    class TokenPosition:
+        def get_curr_position(self) -> str:
+            pass
+
+        def get_end_position(self) -> str:
+            pass
+
+        def get_start_position(self) -> str:
+            pass
+
 
     def __str__(self):
         """Str representation of the object(readability)"""
         if not self.value:
             return f'{self.type}'
+
         return f'{self.type}:{self.value}'
 
     def __repr__(self):
         """str representation of the object, it contain information about the object(information)"""
         return f'Token(type_:{self.type}, value={self.value})'
+
+    def __eq__(self, other):
+        return self.value and self.type == other.value and other.type
+
+
+if __name__ == '__main__':
+    print(sys.getsizeof(Token))
+
